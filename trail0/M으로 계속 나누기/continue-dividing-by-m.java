8e314e32bacb94af04296e8825dde1892825f6a4
baektom <95400988+baektom@users.noreply.github.com>
2026-05-24 22:54:49 +0900

    [Passed] M으로 계속 나누기 (128ms, 10MB)

---
+++ trail0/M으로 계속 나누기/continue-dividing-by-m.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        
+        // Please write your code here.
+        System.out.println(N);
+        while ((N/M) != 0) {
+            N /= M; // N = N / M;
+            System.out.println(N);
+        }
+    }
+}
