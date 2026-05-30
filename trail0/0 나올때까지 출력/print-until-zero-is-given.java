60abf3a7d08f7e6a24bafeadd20dfaa42faa09a0
baektom <95400988+baektom@users.noreply.github.com>
2026-05-30 22:39:57 +0900

    [Passed] 0 나올때까지 출력 (130ms, 10MB)

---
+++ trail0/0 나올때까지 출력/print-until-zero-is-given.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int n = sc.nextInt();
+            if (n == 0) {
+                break;
+            }
+            System.out.println(n);
+        }
+    }
+}
