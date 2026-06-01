8178e994fcd403c6dae3a91b4cf06d3cf46d451d
baektom <95400988+baektom@users.noreply.github.com>
2026-06-02 03:52:57 +0900

    [Passed] 배열의 값을 3배로 (131ms, 10MB)

---
+++ trail0/배열의 값을 3배로/triple-the-values-of-the-array.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int[][] array = new int[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                array[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print((array[i][j] * 3) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
