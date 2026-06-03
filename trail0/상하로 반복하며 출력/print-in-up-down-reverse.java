6eaac8920f6a9f31bef186fb1596ddb70ce6e498
baektom <95400988+baektom@users.noreply.github.com>
2026-06-03 23:50:02 +0900

    [Passed] 상하로 반복하며 출력 (127ms, 10MB)

--- trail0/상하로 반복하며 출력/print-in-up-down-reverse.java
+++ trail0/상하로 반복하며 출력/print-in-up-down-reverse.java
@@ -5,8 +5,7 @@ public class Main {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[][] arr = new int[n][n];
-        
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (j % 2 == 0) {
