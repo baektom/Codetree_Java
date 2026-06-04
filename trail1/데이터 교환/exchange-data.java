b64094fbcd976d666149b7bc1a2d01ff30f3fac0
baektom <95400988+baektom@users.noreply.github.com>
2026-06-05 00:45:05 +0900

    [Passed] 데이터 교환 (99ms, 10MB)

---
+++ trail1/데이터 교환/exchange-data.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 5, b = 6, c = 7;
+        int temp;
+        temp = a;
+        a = c;
+        c = b;
+        b = temp;
+        System.out.println(a + "\n" + b + "\n" + c);
+    }
+}
