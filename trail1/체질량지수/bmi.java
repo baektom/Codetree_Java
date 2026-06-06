a68d612fd2dcd70fbf208771a3269b47e007ae0e
baektom <95400988+baektom@users.noreply.github.com>
2026-06-06 19:38:54 +0900

    [Passed] 체질량지수 (130ms, 10MB)

---
+++ trail1/체질량지수/bmi.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt();
+        int w = sc.nextInt();
+        int b = (10000*w)/(h*h);
+        if (b >= 25) System.out.println(b + "\n" + "Obesity");
+        else System.out.println(b);
+    }
+}
