f8543e953463a295c73f82c79079df69b8ede405
baektom <95400988+baektom@users.noreply.github.com>
2026-06-07 03:53:51 +0900

    [Passed] 알파벳에 따른 평가 (122ms, 10MB)

---
+++ trail1/알파벳에 따른 평가/evaluation-by-alphabet.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+        if (c == 'S') System.out.println("Superior");
+        else if (c == 'A') System.out.println("Excellent");
+        else if (c == 'B') System.out.println("Good");
+        else if (c == 'C') System.out.println("Usually");
+        else if (c == 'D') System.out.println("Effort");
+        else System.out.println("Failure");
+    }
+}
