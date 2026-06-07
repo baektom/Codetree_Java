23b40c89647feb8a6a1b73a01512b7a9eeb4cf6c
baektom <95400988+baektom@users.noreply.github.com>
2026-06-08 03:22:20 +0900

    [Passed] 배열의 특정 위치 찾기 4 (129ms, 10MB)

---
+++ trail1/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        int sum = 0, cnt = 0;
+        for (int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+            if (arr[i] == 0) break;
+            if (arr[i] % 2 == 0) {
+                cnt++;
+                sum += arr[i];
+            }
+        }
+        System.out.print(cnt + " " + sum);
+    }
+}
