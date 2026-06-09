0c61ec77fc9f78f4dfba652a415130245757ff6f
baektom <95400988+baektom@users.noreply.github.com>
2026-06-10 03:15:08 +0900

    [Passed] 정수 입력받아 배수 출력 (137ms, 10MB)

---
+++ trail1/정수 입력받아 배수 출력/print-multiple-of-input.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        for (int i = a; i <= a*5; i++) {
+            if (i % a == 0)
+                System.out.print(i + " ");
+        }
+    }
+}
