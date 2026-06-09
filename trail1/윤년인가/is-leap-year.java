91d36a5d7fcdcbe8d3658199f207c3f2d7fd3077
baektom <95400988+baektom@users.noreply.github.com>
2026-06-09 21:31:00 +0900

    [Passed] 윤년인가 (130ms, 10MB)

---
+++ trail1/윤년인가/is-leap-year.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+        if (y % 4 != 0 || (y % 100 == 0 && y % 400 != 0)) System.out.println("false");
+        else System.out.println("true");
+    }
+}
