75dab6c3f91344a9152d94e3590f48e4967ef126
baektom <95400988+baektom@users.noreply.github.com>
2026-06-09 21:57:21 +0900

    [Passed] 코로나 메뉴얼 (129ms, 10MB)

---
+++ trail1/코로나 메뉴얼/covid-manual.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        char h;
+        int a;
+        int cnt = 0;
+        for (int i = 0; i < 3; i++) {
+            h = sc.next().charAt(0);
+            a = sc.nextInt();
+            if (h == 'Y') {
+                if (a >= 37) {
+                    // System.out.println("A");
+                    cnt++;
+                }
+            } 
+        }
+        if (cnt >= 2) System.out.println("E");
+        else System.out.println("N");
+    }
+}
