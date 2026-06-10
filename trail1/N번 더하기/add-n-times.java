2a8dc03f40fec99c2a34d6d9d25f363ca9e99e06
baektom <95400988+baektom@users.noreply.github.com>
2026-06-10 18:18:16 +0900

    [Passed] N번 더하기 (140ms, 10MB)

---
+++ trail1/N번 더하기/add-n-times.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int n = sc.nextInt();
+        for (int i = a+n; i <= a+n*n; i += n) {
+            System.out.println(i);
+        }
+    }
+}
