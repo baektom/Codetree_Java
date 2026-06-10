03b123d60dc6ae5c7718192ebd48638796124254
baektom <95400988+baektom@users.noreply.github.com>
2026-06-10 18:24:15 +0900

    [Passed] 자연수면 출력 (127ms, 10MB)

---
+++ trail1/자연수면 출력/output-only-natural-number.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (a > 0) {
+            for (int i = 1; i <= b; i++) System.out.print(a);
+        } else System.out.println(0);
+    }
+}
