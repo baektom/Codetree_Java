6a44a3f210f2881af7918f76dee066293b64db88
baektom <95400988+baektom@users.noreply.github.com>
2026-06-10 23:44:42 +0900

    [Passed] A/B 출력 (128ms, 10MB)

---
+++ trail1/A/B 출력/a-divide-b.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int div = a%b;
+        System.out.print(a/b + ".");
+        for (int i = 1; i < 21; i++) {
+            div *= 10;
+            System.out.print(div/b);
+            div %= b;
+        }
+    }
+}
