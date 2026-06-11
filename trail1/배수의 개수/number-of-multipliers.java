a2a2ee48800f7c4cf362aba4e5037fc6278bc67d
baektom <95400988+baektom@users.noreply.github.com>
2026-06-11 23:43:34 +0900

    [Passed] 배수의 개수 (130ms, 10MB)

---
+++ trail1/배수의 개수/number-of-multipliers.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int cnt1 = 0;
+        int cnt2 = 0;
+        for (int i = 1; i <= 10; i++) {
+            int a = sc.nextInt();
+            if (a % 3 == 0) cnt1++;
+            if (a % 5 == 0) cnt2++;
+        }
+        System.out.print(cnt1 + " " + cnt2);
+        
+    }
+}
