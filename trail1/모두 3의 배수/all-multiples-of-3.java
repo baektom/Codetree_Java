a37788e5e02f665c1a7cf68b884c19e920ed4603
baektom <95400988+baektom@users.noreply.github.com>
2026-06-15 22:05:43 +0900

    [Passed] 모두 3의 배수 (127ms, 10MB)

---
+++ trail1/모두 3의 배수/all-multiples-of-3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        boolean satisfied = true;
+        for (int i = 1; i <= 5; i++) {
+            int a = sc.nextInt();
+            if (a % 3 != 0) satisfied = false;
+        }
+        if (satisfied == true) System.out.println(1);
+        else System.out.println(0);
+    }
+}
