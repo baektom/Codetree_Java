e0e19c7726011f65e0c6e1815a07fa41cc682385
baektom <95400988+baektom@users.noreply.github.com>
2026-06-16 23:10:38 +0900

    [Passed] 별표 출력하기 4 (132ms, 10MB)

---
+++ trail1/별표 출력하기 4/print-star-4.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n-i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
