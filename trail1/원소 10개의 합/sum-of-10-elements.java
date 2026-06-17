4d82340a82980bce84f6cae5118777e5e1a3027a
baektom <95400988+baektom@users.noreply.github.com>
2026-06-17 16:48:11 +0900

    [Passed] 원소 10개의 합 (136ms, 10MB)

--- trail1/원소 10개의 합/sum-of-10-elements.java
+++ trail1/원소 10개의 합/sum-of-10-elements.java
@@ -4,11 +4,10 @@ public class Main {
     public static void main(String[] args) {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[10];
         int sum = 0;
         for (int i = 0; i < 10; i++) {
-            arr[i] = sc.nextInt();
-            sum += arr[i];
+            int a = sc.nextInt();
+            sum += a;
         }
         System.out.println(sum);
     }
