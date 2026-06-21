60bdfdd1dd1fb6a6bb853ae81bfa0c390276a4c0
baektom <95400988+baektom@users.noreply.github.com>
2026-06-21 22:10:16 +0900

    [Passed] 숫자들의 배수 (140ms, 10MB)

---
+++ trail1/숫자들의 배수/multiple-of-numbers.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int[] arr = new int[100];
+        int cnt = 0;
+        for (int i = 0; i < 100; i++) {
+            arr[i] = a * (i + 1);
+            System.out.print(arr[i] + " ");
+            if (arr[i] % 5 == 0) cnt++;
+            if (cnt == 2) break;
+        }
+    }
+}
